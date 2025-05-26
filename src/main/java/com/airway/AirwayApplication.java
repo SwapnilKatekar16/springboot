68ff5c44dcce5ad8912e044f464a844735b3c8f4
SwapnilKatekar16 <swapnilkatekar16@gmail.com>
2025-05-26 15:07:25 +0530

    add files

---
+++ src/main/java/com/airway/AirwayApplication.java
@@ -0,0 +1,13 @@
+package com.airway;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class AirwayApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(AirwayApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/airway/AirwayApplicationTests.java
@@ -0,0 +1,13 @@
+package com.airway;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class AirwayApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
