ecf559bce645fcf54287557354b9decfd43d4046
SwapnilKatekar16 <swapnilkatekar16@gmail.com>
2025-05-26 14:37:33 +0530

    add files

---
+++ pipprocject/src/main/java/com/pipproject2/pipprocject/PipprocjectApplication.java
@@ -0,0 +1,13 @@
+package com.pipproject2.pipprocject;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class PipprocjectApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(PipprocjectApplication.class, args);
+	}
+
+}
---
+++ pipprocject/src/test/java/com/pipproject2/pipprocject/PipprocjectApplicationTests.java
@@ -0,0 +1,13 @@
+package com.pipproject2.pipprocject;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class PipprocjectApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
